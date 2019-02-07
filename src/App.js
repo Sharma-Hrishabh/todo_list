90c1e98e000b8999dd0108e87160dabde066549a
Sharma-Hrishabh <30hrs1999@gmail.com>
2019-02-07 23:52:37 +0530

    materialise css added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Todos from './Todos';
 import './App.css';
-
+import AddTodo from './AddTodo';
 class App extends Component {
+  state = {
+    todos : [{id:1,content:"Learn Blockchain"},{id:2,content:"Learn React"}]
+  }
+  deleteTodo = (id) => {
+    // console.log(id);
+    const todos = this.state.todos.filter(todo => {
+        return todo.id !== id;
+    })
+    this.setState({todos:todos})
+  }
+
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos,todo]
+    this.setState({
+      todos:todos
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo-app container">
+      <h1 className="center red-text">Todo's List</h1>
+      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+      <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
